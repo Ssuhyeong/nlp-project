c3ffe10034afdc27f34374ff809b1f06887a9d18
son-su-hyeong <tngud124@gmail.com>
2021-08-10 21:46:33 +0900

    my commit

--- app.py
+++ app.py
@@ -1,14 +1,57 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import json
 from util import correct_sent
+from util.new import crawl_news
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class News(db.Model):
+    __tablename__ = 'news'
+
+
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    link = db.Column(db.Text, nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=False)
+    query = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default = db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default = db.func.current_timestamp(),
+                           onupdate=db.func.current_timestamp())
+
+    def __init__(self, title, link, description):
+        self.title = title
+        self.link = link
+        self.description = description
+
 @app.route('/', methods=['POST'])
+def crawl_news_save():
+    query = request.form['query']
 
-def input():
-    input = request.form["input"]
-    return correct_sent(input)
+    news = crawl_news(query, start=1, offset=10)
 
-app.run(debug=True)
+    news_records = []
+
+    for n in news:
+        news_records.append(News(
+            title = n['title'],
+            link = n['link'],
+            description = n['description']
+        ))
+
+    db.session.add_all(news_records)
+    db.session.commit()
+
+    return 'ok'
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
